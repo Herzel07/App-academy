4c4fca1fe93d6ec89cc7be329b564f315e5473a4
Herzel07 <herzeldandile@gmail.com>
2026-07-19 21:58:21 +0200

    Unit2 Practical Task

---
+++ string_formatter.py
@@ -0,0 +1,12 @@
+first_name = input(user's first name)
+last_name = input(user's last name)
+short_bio = input("bio: ")
+username = f"{first_name[0]} {last_name}"
+print(f"{username.lower()}")
+full_name = first_name + " " + last_name
+print(full_name.title())
+bio = (short_bio.strip())
+no_of_characters = (len(short_bio.strip()))
+updated_bio = bio.replace("I am", "I'm")
+print(f"{full_name.title()} {updated_bio}{no_of_characters}")
+
