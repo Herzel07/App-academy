22c863474a44b5bfe6fced76ea2485a39071dcb1
Herzel07 <herzeldandile@gmail.com>
2026-07-21 21:47:44 +0200

    started calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+num1 = float(input("first number: "))
+num2 = float(input("second number: "))
+addition = num1 + num2
+addition = num1 - num2
+addition = num1 * num2
+addition = num1 / num2
+floor_division = num1 // num2
+modulus = num1 % num2
+if num2 == 0:
+division = "Cannot divide by zero" 
+floor_division = "Cannot divide by zero"
+modulus = "Cannot divide by zero"
+else:
+division = num1 / num2 
+floor_division = num1 // num2 
+modulus = num1 % num2
